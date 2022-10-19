7ed37fc974b2db4ee12ebc854cbd492fccb3b361
Daniel Obraczka <obraczka@informatik.uni-leipzig.de>
2022-10-19 10:50:07 +0200

    add GHA

---
+++ noxfile.py
@@ -0,0 +1,24 @@
+from nox_poetry import Session, session
+
+locations = ["kiez", "tests", "noxfile.py"]
+
+
+@session()
+def lint(session: Session) -> None:
+    args = session.posargs or locations
+    session.install(
+        "pyproject-flake8",
+        "flake8-eradicate",
+        "flake8-isort",
+        "flake8-debugger",
+        "flake8-comprehensions",
+        "flake8-print",
+    )
+    session.run("pflake8", *args)
+
+
+@session()
+def type_checking(session: Session) -> None:
+    args = session.posargs or locations
+    session.install("mypy")
+    session.run("mypy", "--ignore-missing-imports", *args)
